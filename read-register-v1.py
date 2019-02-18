7182bae217d3409c39741583397bb860246386b4
alexanderpollak <Alexander.Pollak.87@gmail.com>
2019-02-18 18:17:46 +0000

    included decode

--- read-register-v1.py
+++ read-register-v1.py
@@ -46,6 +46,82 @@ def encode_field(self, value, mb_type='unit16'):
     return builder.build()
 
 
+def decode(self, raw, size, mb_type, mb_funcall=3):
+    print('decode param (raw=%s, size=%s, mb_type=%s, mb_funcall=%s)' % (raw, size, mb_type, mb_funcall))
+    if mb_funcall == 1:
+        # Read Coil Status (FC=01)
+        print("decoder FC1 (raw: %s)" % raw)
+        decoder = BinaryPayloadDecoder.fromCoils(raw, endian=self.endian)
+    elif mb_funcall == 2:
+        # Read Discrete Input (FC=02)
+        print("decoder FC2 (raw: %s)" % raw)
+        decoder = BinaryPayloadDecoder(raw, endian=self.endian)
+    elif mb_funcall == 3:
+        # Read Holding Registers (FC=03)
+        print("decoder FC3 (raw: %s)" % raw)
+        decoder = BinaryPayloadDecoder.fromRegisters(raw, endian=self.endian)
+    elif mb_funcall == 4:
+        # Read Input Registers (FC=04)
+        print("decoder stub FC4 (raw: %s)" % raw)
+        decoder = BinaryPayloadDecoder(raw, endian=self.endian)
+    else:
+        print("Function call not supported: %s" % mb_funcall)
+        decoder = None
+
+    result = ""
+    if mb_type == 'bitmap':
+        if size == 1:
+            mb_type = 'int8'
+        elif size == 2:
+            mb_type = 'int16'
+        elif size == 2:
+            mb_type = 'int32'
+        elif size == 4:
+            mb_type = 'int64'
+
+    if decoder is None:
+        print("decode none")
+        result = raw
+    else:
+        try:
+            if mb_type == 'string' or mb_type == 'utf8':
+                result = decoder.decode_string(size)
+            # elif mb_type == 'bitmap':
+            #	result = decoder.decode_string(size)
+            elif mb_type == 'datetime':
+                result = decoder.decode_string(size)
+            elif mb_type == 'uint8':
+                result = int(decoder.decode_8bit_uint())
+            elif mb_type == 'int8':
+                result = int(decoder.decode_8bit_int())
+            elif mb_type == 'uint16':
+                result = int(decoder.decode_16bit_uint())
+            elif mb_type == 'int16':
+                result = int(decoder.decode_16bit_int())
+            elif mb_type == 'uint32':
+                result = int(decoder.decode_32bit_uint())
+            elif mb_type == 'int32':
+                result = int(decoder.decode_32bit_int())
+            elif mb_type == 'uint64':
+                result = int(decoder.decode_64bit_uint())
+            elif mb_type == 'int64':
+                result = int(decoder.decode_64bit_int())
+            elif mb_type == 'float32' or mb_type == 'float':
+                result = float(decoder.decode_32bit_float())
+            elif mb_type == 'float64':
+                result = float(decoder.decode_64bit_float())
+            elif mb_type == 'bit':
+                result = int(decoder.decode_bits())
+            elif mb_type == 'bool':
+                result = bool(raw[0])
+            elif mb_type == 'raw':
+                result = raw[0]
+            else:
+                result = raw
+        except ValueError as e:
+            #log.exception(e)
+            result = raw
+    return result
 
 
 SERVER_HOST = "192.168.0.210"
