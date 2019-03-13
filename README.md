dea39f20d98aa1fa0b28342070b313ba86ce6867
alexanderpollak <Alexander.Pollak.87@gmail.com>
2019-03-13 22:11:12 +0000

    included decode

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Solar-Controller-BMS
-This program reads the charge status from US2000B Batteries and controls the XW+ inverter
+This program reads the SOC from Pylontech US2000B Plus Batteries and controls an Schneider XW+ inverter
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Solar-Controller-BMS',
+    version='0.0.1',
+    description='Python Package that Communicates with Pylontech US2000B Plus Batteries and Controls Schneider Conext Components',
+    license='MIT',
+    packages=['source'],
+    author='Dr. Alexander Pollak',
+    author_email='Alexander.Pollak.87@gmail.com',
+    keywords=['Pylontech','US2000B','Schneider','Conext'],
+    url='https://github.com/AlexanderPollak/Solar-Controller-BMS'
+)
