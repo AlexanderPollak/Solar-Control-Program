06d771e60d37df2a6c2e9a94a58399d518b0e672
Alexander Pollak <Alexander.Pollak.87@gmail.com>
2022-09-15 11:00:05 -0700

    download files

--- SCP/main.py
+++ SCP/main.py
@@ -18,6 +18,23 @@ def main():
     Battery_low = 45.5  # Volt
     Battery_hysteresis = 2.0  # Volt
 
+    print('Current Configuration of Control program \n')
+    print('Battery Modules: '+str(Battery_Modules))
+    print('Cadance: ' + str(Cadance))
+    print('SoC inverter ON: ' + str(SoC_high))
+    print('SoC inverter OFF: ' + str(SoC_low))
+    print('\n')
+    print('Battery Low Voltage: ' + str(Battery_low))
+    print('Battery Hysteresis Voltage: '+str(Battery_hysteresis))
+    print('\n')
+    print('Control- Display:' +str(Display))
+    print('Control- Data Log:' + str(Log))
+    print('Control- Inv. Ctrl.:' + str(Control))
+    print('\n')
+
+
+
+
     control(Serial_Port=Serial_Port, Modbus_Host=Modbus_Host, Battery_Modules=Battery_Modules, Cadance=Cadance,\
          Display=Display, Log=Log, Control=Control, SoC_high=SoC_high, SoC_low=SoC_low, Battery_low=Battery_low,\
          Battery_hysteresis=Battery_hysteresis, Log_file_path=Log_File_Path)
