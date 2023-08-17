6c560aab1d627db77583bb6ffe130bdc997b439b
Alexander Pollak <alexander.pollak.87@gmail.com>
2023-08-17 05:44:23 +0000

    added config file function

--- Solar-Control-program.sh
+++ Solar-Control-program.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 # Bash wrapper around python script - to set up environment
-# D. Michalik, 2017
 
-# clustertools
-eval `/software/clustertools/setup.sh`
 
-# for crython
-declare -x PYTHONPATH="/software/clustertools/standard/RHEL_6_x86_64/lib/python2.7/site-packages:/home/sptdaq"
-
-/usr/lib64/nagios/plugins/check_fridge.py
+/usr/local/Solar-Control-Program/venv/bin/python3.10 main.py  /usr/local/Solar-Control-Program/main.py
