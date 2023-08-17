af02b76557022220a74428982f8c2794c8dea3d8
Alexander Pollak <alexander.pollak.87@gmail.com>
2023-08-17 05:57:15 +0000

    renamed main.py to Solar-Control-Program.py

---
+++ Solar-Control-Program.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Bash wrapper around python script - to set up environment
+
+
+/usr/local/Solar-Control-Program/venv/bin/python3.10  /usr/local/Solar-Control-Program/Solar-Control-Program.py
--- Solar-Control-program.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Bash wrapper around python script - to set up environment
-
-
-/usr/local/Solar-Control-Program/venv/bin/python3.10 main.py  /usr/local/Solar-Control-Program/main.py
