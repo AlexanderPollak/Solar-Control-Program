f4b7775c81eedb106ada26361f29157d57f00df3
Alexander Pollak <Alexander.Pollak.87@gmail.com>
2023-08-18 15:46:50 +1200

    changed sql table

--- SQL/bms-data.sql
+++ SQL/bms-data.sql
@@ -18,14 +18,14 @@
 
 
 --
--- Table structure for table `battery_bms`
+-- Table structure for table `pylontech_bms`
 --
 
 DROP TABLE IF EXISTS `pylontech_bms`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `battery_bms` (
-  `ts` datetime NOT NULL CURRENT_TIMESTAMP,
+CREATE TABLE `pylontech_bms` (
+  `ts` datetime NOT NULL,
   `battery` int NOT NULL,
   `soc` float DEFAULT (NULL),
   `voltage` float DEFAULT (NULL),
